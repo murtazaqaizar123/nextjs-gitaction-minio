4fb3a49201b3c225c846cc6856f59a840e5a1684
waliqs <>
2025-08-02 18:26:09 +0500

    Your commit message

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   images: {
     domains: ['imgur.com'],
   },
+  trailingSlash: true, // Required for static export routing
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/shop': { page: '/shop' },
+      '/cart': { page: '/cart' },
+      // You can add dynamic routes manually if needed
+    };
+  },
 };
